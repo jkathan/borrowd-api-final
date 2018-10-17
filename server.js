572008782d4d5b6e3c3a5198cc2a52669ab40a28
Joey Kathan <kathan.joey@gmail.com>
2018-10-17 16:16:49 -0400

    initial commit

--- server.js
+++ server.js
@@ -123,7 +123,7 @@ let server;
 function runServer(databaseUrl, port = PORT) {
 
   return new Promise((resolve, reject) => {
-    mongoose.connect(databaseUrl, { useNewUrlParser: true }, err => {
+    mongoose.connect(databaseUrl, err => {
       if (err) {
         return reject(err);
 
