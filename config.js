3c88d5eb849ccecb82719b726aff146d3e9105f0
Joey Kathan <kathan.joey@gmail.com>
2018-10-17 19:21:06 -0400

    attempting to get login to work

--- config.js
+++ config.js
@@ -2,6 +2,6 @@ exports.DATABASE_URL = process.env.DATABASE_URL ||
                       "mongodb://localhost/borrowd"; 
 exports.TEST_DATABASE_URL = 
 	process.env.TEST_DATABASE_URL || "mongodb://localhost/test-borrowd-app";
-exports.PORT = process.env.PORT || 3000;
+exports.PORT = process.env.PORT || 8080;
 exports.JWT_SECRET = process.env.JWT_SECRET || 'banana';
 exports.JWT_EXPIRY = process.env.JWT_EXPIRY || '7d';
