c40633cd61cf840335d672d1b21e0b0d362a467b
Joey Kathan <kathan.joey@gmail.com>
2018-10-18 09:04:15 -0400

    attempting to get login to work

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 exports.DATABASE_URL = process.env.DATABASE_URL ||
-                      "mongodb://localhost/borrowd"; 
+                      "mongodb://test:test1234@ds231723.mlab.com:31723/borrowd"; 
 exports.TEST_DATABASE_URL = 
 	process.env.TEST_DATABASE_URL || "mongodb://localhost/test-borrowd-app";
 exports.PORT = process.env.PORT || 8080;
