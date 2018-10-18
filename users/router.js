4e0f2a1af78445799f48b8e8ff1e8a9099ccdd07
Joey Kathan <kathan.joey@gmail.com>
2018-10-17 20:23:02 -0400

    attempting to get login to work

--- users/router.js
+++ users/router.js
@@ -138,11 +138,3 @@ router.post('/', jsonParser, (req, res) => {
 // we're just doing this so we have a quick way to see
 // if we're creating users. keep in mind, you can also
 // verify this in the Mongo shell.
-router.get('/', (req, res) => {
-  console.log(res)
-  return User.find()
-    .then(users => res.json(users.map(user => user.serialize())))
-    .catch(err => res.status(500).json({message: 'Internal server error'}));
-});
-
-module.exports = {router};
