2b100158e4112cf60b33e7a3aa7f917bcdb97fab
Joey Kathan <kathan.joey@gmail.com>
2018-10-20 19:21:53 -0400

    deploying to heroku to connect to create react app

--- auth/router.js
+++ auth/router.js
@@ -2,20 +2,10 @@
 const express = require('express');
 const passport = require('passport');
 const bodyParser = require('body-parser');
-const jwt = require('jsonwebtoken');
 const {User} = require('../users/models');
 const config = require('../config');
 const router = express.Router();
 
-const createAuthToken = function(user) {
-  return jwt.sign({user}, config.JWT_SECRET, {
-    subject: user.username,
-    expiresIn: config.JWT_EXPIRY,
-    algorithm: 'HS256'
-  });
-};
-
-const localAuth = passport.authenticate('local', {session: false});
 router.use(bodyParser.json());
 // The user provides a username and password to login
 router.get('/login/:username', (req, res) => {
@@ -27,13 +17,3 @@ router.get('/login/:username', (req, res) => {
       res.status(500).json({ error: 'something went terribly wrong' });
     });
 });
-
-const jwtAuth = passport.authenticate('jwt', {session: false});
-
-// The user exchanges a valid JWT for a new one with a later expiration
-router.post('/refresh', jwtAuth, (req, res) => {
-  const authToken = createAuthToken(req.user);
-  res.json({authToken});
-});
-
-module.exports = {router};
